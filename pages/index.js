9f17cbae4e7134c84de40022d123a0463fdb6896
Lucas Carvalho da Silva <57200163+LucasCarsilva@users.noreply.github.com>
2024-08-22 00:28:48 +0000

    adicionando a pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+function Home() {
+    return <h1>[...]Quando as pessoas descobrem que seus maiores problemas são elas mesmas <br></br>
+                Aí que fica mais difícil de resolvê-los <br></br>
+                Que elas descobrem que são mais fortes do que imaginavam[...]</h1>
+}
+
+export default Home;
