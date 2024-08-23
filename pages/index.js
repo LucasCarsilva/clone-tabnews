64128f4563b66a950e22c54c367374744747118e
Lucas Carvalho da Silva <57200163+LucasCarsilva@users.noreply.github.com>
2024-08-23 13:27:03 +0000

    alteração no texto da pagina principal

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 function Home() {
     return <h1>[...]Quando as pessoas descobrem que seus maiores problemas são elas mesmas <br></br>
                 Aí que fica mais difícil de resolvê-los <br></br>
-                Que elas descobrem que são mais fortes do que imaginavam[...]</h1>
+                Que elas descobrem que são mais fortes do que imaginavam[...]
+                <br></br>
+                - HKSS
+                </h1>
 }
 
 export default Home;
