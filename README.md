0516ec9a95fc9264aea7ff833bd0e9907259a63b
Lucas Carvalho da Silva <57200163+LucasCarsilva@users.noreply.github.com>
2024-09-04 14:51:00 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para criação de uma aplicação de ponta a ponta
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 function Home() {
-    return <h1>[...]Quando as pessoas descobrem que seus maiores problemas são elas mesmas <br></br>
-                Aí que fica mais difícil de resolvê-los <br></br>
-                Que elas descobrem que são mais fortes do que imaginavam[...]
-                <br></br>
-                - HKSS
-                </h1>
+  return (
+    <h1>
+      [...]Quando as pessoas descobrem que seus maiores problemas são elas
+      mesmas <br></br>
+      Aí que fica mais difícil de resolvê-los <br></br>
+      Que elas descobrem que são mais fortes do que imaginavam[...]
+      <br></br>- HKSS
+    </h1>
+  );
 }
 
 export default Home;
